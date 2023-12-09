653a82a1c6d47cde2ba121df7ea1e2c8f2dd088f
Perann <nedjar.prn@gmail.com>
2023-12-09 13:19:25 +0100

    Table 1 finished

--- germansky/OtherSourcesSerialCorrelation/TimeVaryingExpectedReturns.py
+++ germansky/OtherSourcesSerialCorrelation/TimeVaryingExpectedReturns.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def auto_correlation(p,q,sigma, delta,order=1):
-    return ((p+q-1)**(order))/(1+((sigma**2))/(((delta)**2)*(1-p)*(1-q)/((2-p-q)**2)))
+    return ((p+q-1)**(order))/(1+((sigma))/(((delta)**2)*(1-p)*(1-q)/((2-p-q)**2)))
 
 def get_sigma_from_variance(Var,p,q,delta):
     return Var - (delta**2)*((1-p)*(1-q)/(2-p-q)**2)
@@ -10,16 +10,40 @@ def get_sigma_from_variance(Var,p,q,delta):
 
 assert get_sigma_from_variance((0.20**2)/12,1,0,0.05) == (0.20**2)/12
 assert get_sigma_from_variance((0.20**2)/12,0,1,0.05) == (0.20**2)/12
-assert get_sigma_from_variance((0.20**2)/12,0.1,0.1,0.05) == 13/4800
+
 
 def make_pannel_A():
     T = np.zeros((9,9))
     for i in range(9):
         for j in range(9):
-            print(((i+1)/10, (j+1)/10))
-            sigma2 = get_sigma_from_variance((0.20**2)/12,(i+1)/10,(j+1)/10, 0.05)
-            T[i,j] = auto_correlation((i+1)/10, (j+1)/10, sigma2, 0.05, 1)
-            print(T[i,j])
+            p = (i+1)/10
+            q = (j+1)/10
+            sigma2 = get_sigma_from_variance((0.20**2)/12,p,q, 0.05)
+            T[i,j] = round(100*auto_correlation(p,q, sigma2, 0.05, 1),1)
     return T
 
-#print(make_pannel_A())
+def make_pannel_B():
+    T = np.zeros((9,9))
+    for i in range(9):
+        for j in range(9):
+            p = (i+1)/10
+            q = (j+1)/10
+            sigma2 = get_sigma_from_variance((0.50**2)/12,p,q, 0.05)
+            T[i,j] = round(100*auto_correlation(p,q, sigma2, 0.05, 1),1)
+    return T
+
+def make_pannel_C():
+    T = np.zeros((9,9))
+    for i in range(9):
+        for j in range(9):
+            p = (i+1)/10
+            q = (j+1)/10
+            sigma2 = get_sigma_from_variance((0.50**2)/12,p,q, 0.20)
+            T[i,j] = round(100*auto_correlation(p,q, sigma2, 0.20, 1),1)
+    return T
+
+
+
+print(make_pannel_A())
+print(make_pannel_B())
+print(make_pannel_C())

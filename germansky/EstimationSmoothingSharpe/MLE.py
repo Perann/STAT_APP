d6979ad37ebb12cc502aaae9c7914e82a016ee05
Perann <nedjar.prn@gmail.com>
2023-12-10 08:55:26 +0100

    impovment of part 5

--- germansky/EstimationSmoothingSharpe/MLE.py
+++ germansky/EstimationSmoothingSharpe/MLE.py
@@ -7,6 +7,15 @@ alternative_asset_data = pd.read_excel('C:\\Users\\LENOVO\\Desktop\\EnsaeAlterna
 
 Rt = np.array(alternative_asset_data['Private Equity USD Unhedged'])
 mu = np.mean(Rt)
-print(mu)
+
 Xt = Rt - mu
-print(Xt)
+est_Xt = np.zeros(len(Xt))
+est_Xt[0] = 1
+for i in range (1,len(Xt)):
+    est_Xt[i] = np.mean(Xt[:i+1])
+
+delta = np.zeros(len(Xt))
+for i in range(len(Xt)):
+    delta[i] = (Xt[i]-est_Xt[i])**2
+
+print(delta)

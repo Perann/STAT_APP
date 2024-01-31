e31d506afdce4a53e58c2f6d78b78838bcb4352b
Adam El Bernoussi <adam.elbernoussi@ensae.fr>
2024-01-31 16:44:17 +0100

    almost done for the vis with new point

--- main_for_get_visu_to_delete.py
+++ main_for_get_visu_to_delete.py
@@ -19,4 +19,49 @@ if __name__ == "__main__":
     classic_asset_data = pd.read_excel('/Users/adamelbernoussi/Desktop/EnsaeAlternativeSubject/EnsaeAlternativeTimeSeries.xlsx', sheet_name= 'Classic Asset')
 
     # Preprocessing
-    
+    alternative_asset_data = alternative_asset_data[['QUARTER', 'Private Equity USD Unhedged']]
+    alternative_asset_data.dropna(inplace = True)
+    alternative_asset_data['returns PE'] = alternative_asset_data['Private Equity USD Unhedged'].pct_change(fill_method=None)
+    alternative_asset_data.dropna(inplace = True)
+    alternative_asset_data = alternative_asset_data.set_index('QUARTER')
+
+    classic_asset_data = classic_asset_data[['QUARTER', 'Date', 'US Equity USD Unhedged']]
+    classic_asset_data.dropna(inplace = True)
+    classic_asset_data = classic_asset_data.set_index('Date', drop = False).resample('M').last()
+    classic_asset_data['returns US equity'] = classic_asset_data['US Equity USD Unhedged'].pct_change(fill_method=None)
+    classic_asset_data.dropna(inplace = True)
+    classic_asset_data = classic_asset_data.set_index('QUARTER')
+
+    results = classic_asset_data.copy()
+    results = results.merge(alternative_asset_data, how = 'inner', left_index = True, right_index = True).drop(columns = ['US Equity USD Unhedged', 'Private Equity USD Unhedged'])
+    results = results[1:]
+
+    getmansky = GetmanskyModel(2)
+    getmansky.set_default_weights("equal")
+    getmansky.fit(results['returns US equity'].values.reshape(-1, 1), results['returns PE'].values.reshape(-1,1))
+    results['returns unsmoothed'] = getmansky.predict(results['returns US equity'])
+
+    results = results.set_index('Date')
+
+    #print(results)
+
+    # for i in range(len(results['returns PE'])):
+    #     if (i%3 == 0 or i%3 == 1):
+    #         results['returns PE'].iloc[i] = None
+
+    print(results.head(300))
+
+    results['returns unsmoothed TR'] = (results['returns unsmoothed']+1).cumprod()-1
+    results = results.resample('Q').last()
+    results['returns PE TR'] = (results['returns PE']+1).cumprod()-1
+    #results = results.resample('Q').last()
+
+    #print(results)
+
+    # # plotting
+    # results['returns unsmoothed TR'].plot(label = 'Rt PE unsmoothed')
+    # results['returns PE TR'].plot(label = 'Rt PE')
+    # plt.title("Getmansky model with reglin weights PE/US equity")
+    # plt.legend()
+    # #plt.savefig(f'getmansky/output/GetmanskyPres/GetmanskyModel_eqweight_{2}_PE_best.png')
+    # plt.show()

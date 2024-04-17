c54f35622ad3512243221cbf5054d5ae567f1d36
Adam El Bernoussi <adam.elbernoussi@ensae.fr>
2024-04-17 13:51:54 +0200

    malking step furyther almost done for a alpha version

--- web_app/apps/home/dash_app.py
+++ web_app/apps/home/dash_app.py
@@ -2,16 +2,22 @@
 from django_plotly_dash import DjangoDash
 import dash_core_components as dcc
 import dash_html_components as html
+import warnings
+import pandas as pd
+from dash import dash_table
+
 
 app = DjangoDash('SimpleExample')  # Remplace dash.Dash
 
-app.layout = html.Div([
-    html.H1('Bonjour Dash dans Django!'),
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [{'x': [1, 2, 3], 'y': [4, 3, 1], 'type': 'bar', 'name': 'SF'}],
-            'layout': {'title': 'Dash Data Visualization'}
-        }
-    )
-], style={'padding-bottom': "100hv"})
+df = pd.read_excel('./tmp_data_return.xlsx').round(2)
+filtered_columns = [col for col in df.columns if df[col].dtypes ==  "datetime64[ns]"]
+df[filtered_columns] = df[filtered_columns].apply(lambda x: x.dt.strftime('%Y-%m-%d'))
+
+app.layout = dash_table.DataTable(
+    data=df.to_dict('records'),
+    columns=[
+        {'id': c, 'name': c} 
+        for c in df.columns
+    ],
+    style_header={'backgroundColor': 'rgb(60, 65, 105)', "color": "white"},
+)
